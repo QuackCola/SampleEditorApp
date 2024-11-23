7c5dca464ae1f0b92011e57f16edb6474597cbbc
QuackCola <23345567+QuackCola@users.noreply.github.com>
2024-11-23 16:34:15 -0500

    Update MainWindow.cs

--- Libraries/SampleEditorApp/Editor/SampleEditorApp/MainWindow.cs
+++ Libraries/SampleEditorApp/Editor/SampleEditorApp/MainWindow.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-namespace Editor.SampleEditorAppTemplate;
+﻿namespace Editor.SampleEditorAppTemplate;
 
 //[EditorForAssetType( "" )]
 [EditorApp( "Sample Editor App", "category", "Sample editor app" )]
